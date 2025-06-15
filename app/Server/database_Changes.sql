14191a0f3fac480cd561c764913ad7752503f1d0
Mudibo <sean.mudibo@strathmore.edu>
2025-06-15 11:30:21 +0300

    Added a route to delete events, retrieve all events created in the application and to allow guests to RSVP

---
+++ app/Server/database_Changes.sql
@@ -0,0 +1,23 @@
+--The following are the changes made to the database schema
+
+--Removing user_id from the guests table
+Alter TABLE guests
+    DROP COLUMN user_id;
+
+--Adding a UUID column which will be used by the guests for frontend access
+ALTER TABLE guests
+    ADD COLUMN access_code VARCHAR(255) UNIQUE NOT NULL;
+
+--Add a qr_expires_at column to store the expiry of the QR code
+ALTER TABLE guests
+ADD COLUMN qr_expires_at TIMESTAMP;
+
+--Adding start and end time columns to the events table
+ALTER TABLE events
+ADD COLUMN start_time TIME,
+ADD COLUMN end_time TIME;
+-- Adding a constraint to ensure that end_time is greater than start_time if both are provided
+ALTER TABLE events
+ADD CONSTRAINT valid_event_times CHECK (
+  end_time IS NULL OR start_time IS NULL OR end_time > start_time
+);
